b3600d460544e3e179226e529a8742b410b3a760
erler-deerstreet <c.erler@deerstreet.de>
2025-07-23 12:49:10 +0200

    Add new Unity WebGL build with Git LFS

---
+++ Build/PanoTourRE.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3cfdc478b64fb4cca32e21b399c8d7f7c48c3dcd2f74e9b9adc63bcb85fa02f6
+size 26416
--- Build/TestBuild.loader.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5aa088e65ad0dc3c02dac2e56adf03a85253c250885a3c12da004640c40b7e8d
-size 26416
